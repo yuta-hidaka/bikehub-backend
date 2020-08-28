b39d31a08e5ae874e5f06b735aac5246386d2ceb
yuta.hidaka <yuta322@gmail.com>
2020-08-28 14:05:53 +0900

    chnge mariadb image

--- nginx-unit-data/src/bikehub/bikehub/settings.py
+++ nginx-unit-data/src/bikehub/bikehub/settings.py
@@ -12,12 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from .local_settings import *
-from .local_settings import *
-from .local_settings import *
-from .local_settings import *
-from .local_settings import *
 import sys
-from .local_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -31,7 +26,7 @@ sys.path.insert(0, os.path.join(PROJECT_ROOT, "apps"))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['bikehub']
+ALLOWED_HOSTS = ['localhost','bikehub','*']
 
 # Application definition
 
