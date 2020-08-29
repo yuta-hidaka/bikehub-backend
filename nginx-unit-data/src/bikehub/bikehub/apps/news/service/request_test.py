5766d0e3674d890d0d21e96598b90a1563fded7f
yuta.hidaka <yuta322@gmail.com>
2020-08-30 00:20:13 +0900

    Add request tester for rest

---
+++ nginx-unit-data/src/bikehub/bikehub/apps/news/service/request_test.py
@@ -0,0 +1,8 @@
+import requests
+
+url = 'http://133.167.102.92:8888/rest/news/'
+headers = {'Authorization': 'Api-Key hnIVbuI3.4zBiAyiOWFR5abnm26JcBCQ2Niqa6QCu'}
+
+x = requests.get(url,headers=headers)
+
+print(x.text)
