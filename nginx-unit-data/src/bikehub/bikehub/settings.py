fa70af47fcf8832fec3914bba98ea98b3abc5612
yuta.hidaka <yuta322@gmail.com>
2020-08-30 00:11:51 +0900

    change rest responce size

--- nginx-unit-data/src/bikehub/bikehub/settings.py
+++ nginx-unit-data/src/bikehub/bikehub/settings.py
@@ -65,7 +65,7 @@ REST_FRAMEWORK = {
         "rest_framework_api_key.permissions.HasAPIKey",
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 50,
+    'PAGE_SIZE': 10,
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
     ],
