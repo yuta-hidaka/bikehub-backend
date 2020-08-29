c42292ba07214fe7a0d55bbe4879d87337e4e56d
yuta.hidaka <yuta322@gmail.com>
2020-08-30 00:00:08 +0900

    I wrote HasAPIKey wrong place

--- nginx-unit-data/src/bikehub/bikehub/settings.py
+++ nginx-unit-data/src/bikehub/bikehub/settings.py
@@ -61,23 +61,27 @@ JWT_AUTH_COOKIE = 'bikehub-auth'
 # ACCOUNT_ADAPTER = 'users.adapter.AccountAdapter'
 # Pagination
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 50,
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+        ],
     'DEFAULT_THROTTLE_CLASSES': [
         'rest_framework.throttling.AnonRateThrottle',
         'rest_framework.throttling.UserRateThrottle'
+    ],
+        'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework_api_key.permissions.HasAPIKey',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_THROTTLE_RATES': {
         'anon': '1000/day',
         'user': '1000/day'
     },
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        "rest_framework_api_key.permissions.HasAPIKey",
-        'rest_framework.authentication.BasicAuthentication',
-        'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-    )
 }
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
