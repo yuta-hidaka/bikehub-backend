b067f3bbb871d261e2526920a9f890b918ba3233
yuta.hidaka <yuta322@gmail.com>
2020-08-30 20:37:30 +0900

    all CROS ORIGIN

--- nginx-unit-data/src/bikehub/bikehub/settings.py
+++ nginx-unit-data/src/bikehub/bikehub/settings.py
@@ -88,6 +88,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = [
     'http://localhost',
+    '180.50.20.37',
 ]
 CORS_ORIGIN_REGEX_WHITELIST = [
     'http://localhost',
