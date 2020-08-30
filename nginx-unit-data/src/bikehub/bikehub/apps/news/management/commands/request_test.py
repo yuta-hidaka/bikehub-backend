a9e581709dabf1ca8e60d99d41a1a58b39e9f945
yuta.hidaka <yuta322@gmail.com>
2020-08-30 09:28:17 +0900

    change requestr test palce and create custom command

---
+++ nginx-unit-data/src/bikehub/bikehub/apps/news/management/commands/request_test.py
@@ -0,0 +1,12 @@
+import requests
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    def handle(self, **options):
+        url = 'http://133.167.102.92:8888/rest/news/'
+        headers = {'Authorization': 'Api-Key hnIVbuI3.4zBiAkyiOWFR5abnm26JcBCQ2Niqa6QCu'}
+
+        x = requests.get(url,headers=headers)
+
+        print(x.text)
