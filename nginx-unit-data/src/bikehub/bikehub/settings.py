678e13830c695737eaac3bd05dda6432304d8a1f
yuta.hidaka <yuta322@gmail.com>
2020-09-06 01:52:50 +0900

    test

--- nginx-unit-data/src/bikehub/bikehub/settings.py
+++ nginx-unit-data/src/bikehub/bikehub/settings.py
@@ -80,7 +80,9 @@ JWT_AUTH_COOKIE = 'bikehub-auth'
 # Pagination
 REST_SESSION_LOGIN = False
 REST_AUTH_REGISTER_SERIALIZERS = {
-    'USER_DETAILS_SERIALIZER': 'rest.serializer.users.UserViewSerializer',
+    'USER_DETAILS_SERIALIZER': 'rest.serializer.users.UserViewSerializer'
+}
+REST_AUTH_SERIALIZERS = {
     'REGISTER_SERIALIZER': 'rest.serializer.users.UserRegistrationSerializer'
 }
 REST_FRAMEWORK = {
