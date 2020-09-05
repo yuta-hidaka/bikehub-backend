61d64fa0c201c90fb5435057c0c0444da47e224d
yuta.hidaka <yuta322@gmail.com>
2020-09-05 17:47:58 +0900

    test

--- nginx-unit-data/src/bikehub/bikehub/apps/users/context_processors.py
+++ nginx-unit-data/src/bikehub/bikehub/apps/users/context_processors.py
@@ -1,3 +1,13 @@
+from django.contrib import admin
+
+
 def admin_header_processor(request):
-    site_header = getattr(admin.sites, 'site_header')  # get site header text. For django 2.X it should be 
-    return {"site_header": site_header}
+    site_header = getattr(admin.sites, 'site_header')
+    index_title = getattr(admin.sites, 'index_title')
+    site_title = getattr(admin.sites, 'site_title')
+
+    return {
+        "site_header": site_header,
+        "site_header": index_title,
+        "site_header": site_title
+    }
