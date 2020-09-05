1748c7d1293a038b4ce15143fdaa6c191ffbd36d
yuta.hidaka <yuta322@gmail.com>
2020-09-05 17:44:05 +0900

    test

--- nginx-unit-data/src/bikehub/bikehub/apps/users/admin.py
+++ nginx-unit-data/src/bikehub/bikehub/apps/users/admin.py
@@ -1,3 +1,5 @@
+from django.utils.translation import ugettext_lazy
+from django.contrib.admin import AdminSite
 from django.contrib import admin
 from .models import *
 
@@ -19,3 +21,17 @@ class CustomUserAdmin(admin.ModelAdmin):
 admin.site.register(
     CustomUser, CustomUserAdmin
 )
+
+
+class MyAdminSite(AdminSite):
+    # Text to put at the end of each page's <title>.
+    site_title = ugettext_lazy('My site admin')
+
+    # Text to put in each page's <h1> (and above login form).
+    site_header = ugettext_lazy('My administration')
+
+    # Text to put at the top of the admin index page.
+    index_title = ugettext_lazy('Site administration')
+
+
+admin_site = MyAdminSite()
