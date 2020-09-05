e5509ae8ae454dcf1a358a01b9a8dfefb45e68fb
yuta.hidaka <yuta322@gmail.com>
2020-09-05 19:08:44 +0900

    test

--- nginx-unit-data/src/bikehub/bikehub/settings.py
+++ nginx-unit-data/src/bikehub/bikehub/settings.py
@@ -133,8 +133,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'bikehub', 'templates'),
-            os.path.join(BASE_DIR, 'bikehub', 'apps', 'templates'),
+            # os.path.join(BASE_DIR, 'bikehub', 'templates'),
+            # os.path.join(BASE_DIR, 'bikehub', 'apps', 'templates'),
             os.path.join(BASE_DIR, 'apps', 'templates'),
             # os.path.join(BASE_DIR, 'templates'),
             # os.path.join(BASE_DIR, 'templates', 'allauth'),
