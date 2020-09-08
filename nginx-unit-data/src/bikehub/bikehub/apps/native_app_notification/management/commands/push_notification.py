ab9e750ceb9f4104f5ffd25078a7cb24811b814d
yuta.hidaka <yuta322@gmail.com>
2020-09-08 10:08:17 +0900

    chnage token

--- nginx-unit-data/src/bikehub/bikehub/apps/native_app_notification/management/commands/push_notification.py
+++ nginx-unit-data/src/bikehub/bikehub/apps/native_app_notification/management/commands/push_notification.py
@@ -5,26 +5,25 @@ from django.conf import settings
 from pytz import timezone
 
 
-
 class Command(BaseCommand):
     def handle(self, **options):
-        notifications = Notifications() 
+        notifications = Notifications()
         news = News.objects.order_by('-created_at').first()
         settings_time_zone = timezone(settings.TIME_ZONE)
         created_at = news.created_at.astimezone(settings_time_zone)
 
         data = {
-        'news_id': str(news.news_id),
-        'title': news.title,
-        'author': news.site.name,
-        'imgUrl': news.featured_image,
-        'summary': news.summary,
-        'url': news.url,
-        'created_at': str(created_at)
+            'news_id': str(news.news_id),
+            'title': news.title,
+            'author': news.site.name,
+            'imgUrl': news.featured_image,
+            'summary': news.summary,
+            'url': news.url,
+            'created_at': str(created_at)
         }
 
         notifications.send_notification(
-            token='ExponentPushToken[_-uFkyHGJotz8bhjvBQnWa]',
+            token='ExponentPushToken[B9opxUHaIB7atnKGr_Te2c]',
             title='新しいニュースが届きました！',
             body=f'{news.title}',
             data=data
