a1eb615ce1323d9a7c0a58f3765b8dd6bbd75516
yuta.hidaka <yuta322@gmail.com>
2020-09-09 10:44:57 +0900

    Fix Bug

--- nginx-unit-data/src/bikehub/bikehub/apps/rest/serializer/fuel_consumption.py
+++ nginx-unit-data/src/bikehub/bikehub/apps/rest/serializer/fuel_consumption.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 
 class CountrySerializer(serializers.ModelSerializer):
     class Meta:
-        model = Country(read_only=True)
+        model = Country
         fields = [
             'country_id',
             'country',
