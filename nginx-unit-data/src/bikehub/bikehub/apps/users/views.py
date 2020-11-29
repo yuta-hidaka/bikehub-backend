295cd0996c2010679c6b0ed90271137505ab608f
yuta.hidaka <yuta322@gmail.com>
2020-11-29 12:29:11 +0900

    add: redirects

--- nginx-unit-data/src/bikehub/bikehub/apps/users/views.py
+++ nginx-unit-data/src/bikehub/bikehub/apps/users/views.py
@@ -1,8 +1,10 @@
 from django.http import Http404
 from django.shortcuts import render
+from django.shortcuts import redirect
 
 
 def index(request):
+    return redirect('https://www.bikehub.app/')
     return render(request, 'account/index.html')
 
 
