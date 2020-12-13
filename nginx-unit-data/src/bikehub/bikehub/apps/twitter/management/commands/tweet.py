453ef90bee8505e8858789764f9f7f67aa3b9ec5
yuta.hidaka <yuta322@gmail.com>
2020-12-13 16:20:33 +0900

    fix

--- nginx-unit-data/src/bikehub/bikehub/apps/twitter/management/commands/tweet.py
+++ nginx-unit-data/src/bikehub/bikehub/apps/twitter/management/commands/tweet.py
@@ -42,7 +42,6 @@ class Command(BaseCommand):
             title = news.title[:((len(news.title)) - (diff + 10))] + '...'
             message = f'【BikeHubニュース便】\n - {author} - {title} \n #バイク好きと繋がりたい #バイクのある生活 #バイクのニュース #BikeHub\n'
 
-        print(f'{message}{url}')
         api.update_status(status=f'{message}{url}')
 
         news.is_posted = True
